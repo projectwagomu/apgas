ef856d50c46c22e4d75689df2f794c585737b16f
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2023-04-19 21:17:42 +0900

    Cleared remaining warnings from the project

--- src/main/java/apgas/util/ConsolePrinter.java
+++ src/main/java/apgas/util/ConsolePrinter.java
@@ -11,110 +11,111 @@ import java.time.format.DateTimeFormatter;
 
 public class ConsolePrinter implements Serializable {
 
-  private static final boolean PRINT = Configuration.APGAS_CONSOLEPRINTER.get();
+	/** Serial Version UID */
+	private static final long serialVersionUID = 6535048433179703185L;
 
-  private static final ConsolePrinter instance = new ConsolePrinter();
+	private static final boolean PRINT = Configuration.APGAS_CONSOLEPRINTER.get();
 
-  private static final long startTime = System.nanoTime();
+	private static final ConsolePrinter instance = new ConsolePrinter();
 
-  public static synchronized ConsolePrinter getInstance() {
-    return ConsolePrinter.instance;
-  }
+	public static synchronized ConsolePrinter getInstance() {
+		return ConsolePrinter.instance;
+	}
 
-  public synchronized void printlnWithoutAPGAS(String output) {
-    if (PRINT == true) {
-      String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
-      String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
-      System.out.println("[" + time + "] (in " + callerName + ") : " + output);
-    }
-  }
+	public synchronized void printlnWithoutAPGAS(String output) {
+		if (PRINT == true) {
+			String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
+			String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
+			System.out.println("[" + time + "] (in " + callerName + ") : " + output);
+		}
+	}
 
-  public synchronized void println(String output) {
-    if (PRINT == true) {
-      String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
-      String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
-      String host = "localhost";
-      try {
-        host = InetAddress.getLocalHost().getHostName();
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-      System.out.println(
-          here() + "@" + host + " [" + time + "] (in " + callerName + ") : " + output);
-    }
-  }
+	public synchronized void println(String output) {
+		if (PRINT == true) {
+			String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
+			String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
+			String host = "localhost";
+			try {
+				host = InetAddress.getLocalHost().getHostName();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.out.println(
+					here() + "@" + host + " [" + time + "] (in " + callerName + ") : " + output);
+		}
+	}
 
-  public synchronized void printlnErr(String output) {
-    if (PRINT == true) {
-      String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
-      String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
-      System.err.println(here() + " [" + time + "] (in " + callerName + ") : " + output);
-    }
-  }
+	public synchronized void printlnErr(String output) {
+		if (PRINT == true) {
+			String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
+			String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
+			System.err.println(here() + " [" + time + "] (in " + callerName + ") : " + output);
+		}
+	}
 
-  public synchronized void print(String output) {
-    if (PRINT == true) {
-      String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
-      String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
-      System.out.print(here() + " [" + time + "] (in " + callerName + ") : " + output);
-    }
-  }
+	public synchronized void print(String output) {
+		if (PRINT == true) {
+			String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
+			String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
+			System.out.print(here() + " [" + time + "] (in " + callerName + ") : " + output);
+		}
+	}
 
-  public synchronized void printErr(String output) {
-    if (PRINT == true) {
-      String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
-      String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
-      System.err.print(here() + " [" + time + "] (in " + callerName + ") : " + output);
-    }
-  }
+	public synchronized void printErr(String output) {
+		if (PRINT == true) {
+			String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
+			String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
+			System.err.print(here() + " [" + time + "] (in " + callerName + ") : " + output);
+		}
+	}
 
-  public synchronized void remotePrintln(int source, String output) {
-    if (PRINT == true) {
-      String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
-      String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
-      System.out.println(
-          place(source)
-              + " (in "
-              + callerName
-              + " at "
-              + here().id
-              + ") ["
-              + time
-              + "]: "
-              + output);
-    }
-  }
+	public synchronized void remotePrintln(int source, String output) {
+		if (PRINT == true) {
+			String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
+			String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
+			System.out.println(
+					place(source)
+					+ " (in "
+							+ callerName
+							+ " at "
+							+ here().id
+							+ ") ["
+							+ time
+							+ "]: "
+							+ output);
+		}
+	}
 
-  public synchronized void remotePrintlnErr(int source, String output) {
-    if (PRINT == true) {
-      String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
-      String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
-      System.err.println(
-          place(source)
-              + " (in "
-              + callerName
-              + " at "
-              + here().id
-              + ") ["
-              + time
-              + "]: "
-              + output);
-    }
-  }
+	public synchronized void remotePrintlnErr(int source, String output) {
+		if (PRINT == true) {
+			String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
+			String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
+			System.err.println(
+					place(source)
+					+ " (in "
+							+ callerName
+							+ " at "
+							+ here().id
+							+ ") ["
+							+ time
+							+ "]: "
+							+ output);
+		}
+	}
 
-  public synchronized boolean getStatus() {
-    return PRINT;
-  }
+	public synchronized boolean getStatus() {
+		return PRINT;
+	}
 
-  public synchronized void printlnAlways(String output) {
-    String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
-    String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
-    String host = "localhost";
-    try {
-      host = InetAddress.getLocalHost().getHostName();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    System.out.println(here() + "@" + host + " [" + time + "] (in " + callerName + ") : " + output);
-  }
+	public synchronized void printlnAlways(String output) {
+		String callerName = Thread.currentThread().getStackTrace()[2].getMethodName();
+		String time = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME);
+		String host = "localhost";
+		try {
+			host = InetAddress.getLocalHost().getHostName();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(here() + "@" + host + " [" + time + "] (in " + callerName + ") : " + output);
+	}
 }
--- src/main/java/apgas/util/ResilientPlaceGroup.java
+++ src/main/java/apgas/util/ResilientPlaceGroup.java
@@ -10,69 +10,71 @@ import java.util.NoSuchElementException;
 
 public class ResilientPlaceGroup implements Serializable {
 
-  private final Place[] array;
-  private int max;
+	/** Serial Version UID */
+	private static final long serialVersionUID = -4918341729994198948L;
+	private final Place[] array;
+	private int max;
 
-  public ResilientPlaceGroup(int size) {
-    array = new Place[size];
-    int id = 0;
-    for (final Place p : places()) {
-      array[id++] = p;
-      if (id == size) {
-        max = p.id;
-        return;
-      }
-    }
-    System.err.println("[APGAS] Too few places to initialize the ResilientPlaceGroup. Aborting.");
-    System.exit(1);
-  }
+	public ResilientPlaceGroup(int size) {
+		array = new Place[size];
+		int id = 0;
+		for (final Place p : places()) {
+			array[id++] = p;
+			if (id == size) {
+				max = p.id;
+				return;
+			}
+		}
+		System.err.println("[APGAS] Too few places to initialize the ResilientPlaceGroup. Aborting.");
+		System.exit(1);
+	}
 
-  public void fix() {
-    final List<? extends Place> places = places();
-    final Iterator<? extends Place> it = places.iterator();
-    Place spare;
-    try {
-      for (int id = 0; id < array.length; ++id) {
-        if (!places.contains(array[id])) {
-          for (; ; ) {
-            spare = it.next();
-            if (spare.id > max) {
-              break;
-            }
-          }
-          array[id] = spare;
-          max = spare.id;
-        }
-      }
-    } catch (final NoSuchElementException e) {
-      System.err.println("[APGAS] Too few places to fix the ResilientPlaceGroup. Aborting.");
-      System.exit(1);
-    }
-  }
+	public void fix() {
+		final List<? extends Place> places = places();
+		final Iterator<? extends Place> it = places.iterator();
+		Place spare;
+		try {
+			for (int id = 0; id < array.length; ++id) {
+				if (!places.contains(array[id])) {
+					for (; ; ) {
+						spare = it.next();
+						if (spare.id > max) {
+							break;
+						}
+					}
+					array[id] = spare;
+					max = spare.id;
+				}
+			}
+		} catch (final NoSuchElementException e) {
+			System.err.println("[APGAS] Too few places to fix the ResilientPlaceGroup. Aborting.");
+			System.exit(1);
+		}
+	}
 
-  public Place get(int id) {
-    return array[id];
-  }
+	public Place get(int id) {
+		return array[id];
+	}
 
-  public int size() {
-    return array.length;
-  }
+	public int size() {
+		return array.length;
+	}
 
-  public boolean contains(Place place) {
-    for (int id = 0; id < array.length; ++id) {
-      if (array[id].id == place.id) {
-        return true;
-      }
-    }
-    return false;
-  }
+	public boolean contains(Place place) {
+		for (int id = 0; id < array.length; ++id) {
+			if (array[id].id == place.id) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-  public int indexOf(Place place) {
-    for (int id = 0; id < array.length; ++id) {
-      if (array[id].id == place.id) {
-        return id;
-      }
-    }
-    return -1;
-  }
+	public int indexOf(Place place) {
+		for (int id = 0; id < array.length; ++id) {
+			if (array[id].id == place.id) {
+				return id;
+			}
+		}
+		return -1;
+	}
 }
