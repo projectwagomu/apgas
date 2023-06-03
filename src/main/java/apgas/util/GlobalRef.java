8961c2a1deccecbfc0aeb92d004e0702f44cee09
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2023-06-03 17:26:14 +0900

    Added Copyright headers and LICENSE file

--- src/main/java/apgas/util/GlobalRef.java
+++ src/main/java/apgas/util/GlobalRef.java
@@ -1,12 +1,12 @@
 /*
- *  This file is part of the X10 project (http://x10-lang.org).
+ * Copyright (c) 2023 Wagomu project.
  *
- *  This file is licensed to You under the Eclipse Public License (EPL);
- *  You may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *      http://www.opensource.org/licenses/eclipse-1.0.php
+ * This program and the accompanying materials are made available to you under
+ * the terms of the Eclipse Public License 1.0 which accompanies this
+ * distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v10.html
  *
- *  (C) Copyright IBM Corporation 2006-2016.
+ * SPDX-License-Identifier: EPL-1.0
  */
 
 package apgas.util;
--- src/test/java/apgas/runtime/APGASCoreTests.java
+++ src/test/java/apgas/runtime/APGASCoreTests.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2023 Wagomu project.
+ *
+ * This program and the accompanying materials are made available to you under
+ * the terms of the Eclipse Public License 1.0 which accompanies this
+ * distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v10.html
+ *
+ * SPDX-License-Identifier: EPL-1.0
+ */
 package apgas.runtime;
 
 import static apgas.Constructs.async;
